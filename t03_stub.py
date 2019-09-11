6d985387e7a5003eefbddb74b4373ddfcb88c358
maynardb <maynardb@berea.edu>
2019-09-11 08:25:11 -0400

    finished main function and completed the assignment

--- t03_stub.py
+++ t03_stub.py
@@ -1,9 +1,9 @@
 #################################################################################
 # Author:Ben, Concepta
-# Username:
+# Username:maynardb, mjolimac
 #
-# Assignment:
-# Purpose:
+# Assignment: T03: Boustrophedon Turtles and Functions
+# Purpose: To draw a square and fill it in with a boustrophedon pattern
 #################################################################################
 # Acknowledgements:
 #
@@ -36,7 +36,7 @@ def draw_pattern(t, l, h):
     param h: height of the design
     return: none
     """
-    for i in range(12):
+    for i in range(11):
         t.forward(l)
         t.left(90)
         t.forward(h)
@@ -50,17 +50,32 @@ def draw_pattern(t, l, h):
 
 def main():
     """
-    Docstring for main
+    sets up attributes and calls functions
+
+    return: none
     """
     # ...
-    test = turtle.Turtle()
-    test.penup()
-    test.setpos(250, 250)
-    test.left(180)
-    test.pendown()
+    wn = turtle.Screen()
+    scott = turtle.Turtle()
+    scott.pensize(20)
+    scott.penup()
+    scott.setpos(250, 250)
+    scott.left(180)
+    scott.pendown()
 
-    outside_square(test, 500)            # Function call to function_1
-    draw_pattern(test, 480, 40)            # Function call to function_2
+    outside_square(scott, 500)              # Function call to function_1
+    scott.penup()
+    scott.setposition(230, 230)
+    scott.pendown()
+    scott.color("red")
+    draw_pattern(scott, 460, 20)            # Function call to function_2
+    scott.forward(460)
+    scott.left(90)
+    scott.forward(20)
+    scott.left(90)
+    scott.forward(460)
+
+    wn.exitonclick()
 
 
 main()
